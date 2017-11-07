0550b8712f7671dd51431ebe7a41937eeb361442
liuyass <liuya41@live.com>
2017-11-07 18:35:33 +0800

    change pc

--- app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
+++ app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
@@ -1,11 +1,10 @@
 package cn.techscan.gd.diy_view;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
+
+import java.util.Arrays;
 
 public class Main2Act extends AppCompatActivity {
 
@@ -15,56 +14,7 @@ public class Main2Act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Main2Act.this, MainActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopService(new Intent(Main2Act.this, FloatWindowService.class));
-            }
-        });
-        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= 23) {
-                    if (!Settings.canDrawOverlays(getApplicationContext())) {
-                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-                        startActivity(intent);
-                        return;
-                    } else {
-                        startService(new Intent(Main2Act.this, FloatWindowService.class));
-                    }
-                } else {
-                    startService(new Intent(Main2Act.this, FloatWindowService.class));
-                }
-
-            }
-        });
+        String str = "1|2||3";
+        Log.d("TAG", "onCreate: " + Arrays.toString(str.split("\\|")));
     }
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        switch (event.getAction()) {
-//            case MotionEvent.ACTION_DOWN:
-//                Log.d(TAG, "onTouchEvent: " + event.getRawX() + "|" + event.getY());
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                Log.d(TAG, "onTouchEvent: " + event.getRawX() + "|" + event.getY());
-//                break;
-//            case MotionEvent.ACTION_UP:
-//                Log.d(TAG, "onTouchEvent: " + event.getRawX() + "|" + event.getY());
-//                break;
-//            default:
-//
-//                break;
-//        }
-//        return super.onTouchEvent(event);
-//    }
 }
