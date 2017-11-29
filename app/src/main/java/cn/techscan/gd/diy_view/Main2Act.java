066e74b6c9ed437bf91cb38a9c2dd712dd7c5c10
liuyass <liuya41@live.com>
2017-11-29 17:50:18 +0800

    change pc

--- app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
+++ app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
@@ -2,9 +2,6 @@ package cn.techscan.gd.diy_view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.util.Arrays;
 
 public class Main2Act extends AppCompatActivity {
 
@@ -14,8 +11,6 @@ public class Main2Act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        String str = "1|2||3";
-        Log.d("TAG", "onCreate: " + Arrays.toString(str.split("\\|")));
-        // this is my change
+
     }
 }
