38c731f7678c5ba0af117e909a53329b4ed9e1b7
liuyasss <liuya41@live.com>
2019-04-06 17:16:49 +0800

    2

--- app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
+++ app/src/main/java/cn/techscan/gd/diy_view/Main2Act.java
@@ -12,5 +12,6 @@ public class Main2Act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         // 1
+        // 2
     }
 }
